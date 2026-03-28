2e6f561f305ec7bfa50232fce57f018270f0aed9
Nick <Nickvuletich23@gmail.com>
2026-03-28 13:31:52 -0400

    added waste calc

--- logic/logic.py
+++ logic/logic.py
@@ -2,13 +2,35 @@
 # Programmer: Nicholas Vuletich
 # Date: 3-28-26
 
-#----------Waste Claculator----------
-def waste_calculator():
-    
+#----------Test data----------
+items = [
+    {"name": "Chips" ,"price": 5.99, "essential": False},
+    {"name": "Pizza(10in)" ,"price": 11.99, "essential": False},
+    {"name": "Chicken" ,"price": 11.43, "essential": True}
+]
 
+
+#----------Gemini Classifier----------
+
+
+#----------Waste Claculator----------
+def waste_calculator(items):
+    waste = 0
+    for item in items:
+        if item["essential"] is False:
+            waste += item["price"]
+    return waste
 
 #----------Projections----------
 
 #----------Trends----------
 
 #----------Insight generation----------
+
+
+#----------Testing----------
+
+if __name__ == "__main__":
+    waste = waste_calculator(items)
+    print(waste)
+    
