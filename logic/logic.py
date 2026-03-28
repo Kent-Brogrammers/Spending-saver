2879507dd38181353a7af923b06ebde2474d06be
Nick <Nickvuletich23@gmail.com>
2026-03-28 13:05:02 -0400

    adding

--- logic/logic.py
+++ logic/logic.py
@@ -2,3 +2,13 @@
 # Programmer: Nicholas Vuletich
 # Date: 3-28-26
 
+#----------Waste Claculator----------
+def waste_calculator():
+    
+
+
+#----------Projections----------
+
+#----------Trends----------
+
+#----------Insight generation----------
