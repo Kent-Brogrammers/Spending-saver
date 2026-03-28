a5f575bd13207f8f845cb6ac1ff98073bf82d478
ChrisV3 <67931838+ChrisVul3@users.noreply.github.com>
2026-03-28 14:25:44 -0400

    Front end layour redone

---
+++ front-end/SpendingSaver/app/SpendingSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingSaverApp.swift
+//  SpendingSaver
+//
+//  Created by Chris Vuletich on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SpendingSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
