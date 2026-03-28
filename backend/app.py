14bd84c61bc70e7aafe19321c5a447076900dea0
Connor <connorrb1111@gmail.com>
2026-03-28 14:03:31 -0400

    Moved env

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import snowflake.connector
 
 app = Flask(__name__)
+
+SNOWFLAKE_CONFIG = {
+    'user': 'YOUR_USERNAME',         # your default user
+    'password': 'YOUR_PASSWORD',     # your Snowflake password
+    'account': 'tpilzjq',            # from the URL
+    'warehouse': 'MY_WAREHOUSE',     # your warehouse
+    'database': 'SPENDING_SAVER_DB', # from URL
+    'schema': 'PUBLIC'               # default schema
+}
 
 @app.route('/')
 def main():
